4fbca3d5e7956338ced537e80633694bc669dd66
Kevin Grandon <kevingrandon@yahoo.com>
2014-09-12 12:44:51 -0700

    Steal basic sanity checking from TC.

--- routes/pull_request.js
+++ routes/pull_request.js
@@ -1,5 +1,29 @@
 module.exports = function(runtime) {
-  return function* () {
-    console.log('Pull request.');
+  return function * () {
+
+    var body = this.request.body;
+    if (!body) {
+      return this.throw(400, 'Must contain a body');
+    }
+
+    var repository = body.repository;
+    var pullRequest = body.pull_request;
+    var action = body.action;
+
+    if (!pullRequest) {
+      return this.throw(400, 'Invalid or missing pull request data');
+    }
+
+    // We don't need to do anything on closed PRs.
+    if (action === 'closed') {
+      this.status = 200;
+      return;
+    }
+
+    // XXX: Validate pull request message.
+    // XXX: Comment on bugzilla.
+
+    // Everything was ok.
+    this.status = 200;
   };
 };
