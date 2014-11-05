4a0d7f9767c20faaad3e603a0935c3b68f61299f
Kevin Grandon <kevingrandon@yahoo.com>
2014-11-04 20:05:07 -0800

    Bug 1080280 - Validate commit messages

--- routes/pull_request.js
+++ routes/pull_request.js
@@ -1,6 +1,7 @@
 var bugzilla = require('../lib/bugzilla');
 var debug = require('debug')('autolander:route:pull_request');
 var github = require('../lib/github');
+var validator = require('../lib/github/validator');
 var thunkify = require('thunkify');
 
 module.exports = function(runtime) {
@@ -34,15 +35,10 @@ module.exports = function(runtime) {
 
     // Validate that we have a bug number formatted to: "Bug xxxx - "
     // For now allow preceding characters to allow for reverts.
-    var prTitle = pull.title;
-    var bugId = prTitle.match(/^.*Bug\s{1}([0-9]{5,})\s{1}-{1}\s{1}.*/);
-    if (!bugId || !bugId[1]) {
-      var repoParts = pull.base.repo.full_name.split('/');
-      yield github.addComment(runtime, repoParts[0], repoParts[1], pull.number, github.COMMENTS.NO_BUG_FOUND);
-      debug('Bug ID not found.');
+    var bugId = yield validator.pullRequestHasBug(runtime, pull);
+    if (!bugId) {
       return;
     }
-    bugId = bugId[1];
 
     yield bugzilla.attachPullRequest(runtime, bugId, pull);
     yield runtime.pulseApi.subscribe(runtime, bugId);
---
+++ test/validate_commit_has_bug_number_test.js
@@ -0,0 +1,53 @@
+var assert = require('assert');
+var co = require('co');
+var fs = require('fs');
+var helper = require('./helper');
+var jsTemplate = require('json-templater/object');
+var slugid = require('slugid');
+
+var commitContent = require('./support/commit_content');
+var createBug = require('./support/create_bug');
+var createPullRequest = require('./support/create_pull_request');
+var branchFromMaster = require('./support/branch_from_master');
+var reviewAttachment = require('./support/review_attachment');
+var setCheckinNeeded = require('./support/set_checkin_needed');
+var waitForAttachments = require('./support/wait_for_attachments');
+var waitForCheckinNeededRemoved = require('./support/wait_for_checkin_needed_removed');
+var waitForPullComments = require('./support/wait_for_pull_comments');
+
+suite('validates commit message title > ', function() {
+  var runtime;
+
+  suiteSetup(co(function * () {
+    runtime = yield require('./support/runtime')()
+    return yield helper.setup(runtime);
+  }));
+
+  suiteTeardown(co(function * () {
+    return yield helper.teardown(runtime);
+  }));
+
+  test('when missing a bug number', co(function * () {
+    var taskgraph = fs.readFileSync(__dirname + '/fixtures/tc_success/taskgraph.json', 'utf-8');
+    taskgraph = jsTemplate(taskgraph, {
+      taskId: slugid.v4()
+    });
+    yield commitContent(runtime, 'master', 'taskgraph.json', taskgraph, 'Initial commit.');
+
+    var bug = yield createBug(runtime);
+    var ref = yield branchFromMaster(runtime, 'branch1');
+
+    yield commitContent(runtime, 'branch1', 'foo.txt', 'foo', 'no bug number =(');
+    var pull = yield createPullRequest(runtime, 'branch1', 'master', 'Bug ' + bug.id + ' - valid title, but not the commit message');
+
+    var attachments = yield waitForAttachments(runtime, bug.id);
+    yield reviewAttachment(runtime, attachments[0]);
+    yield setCheckinNeeded(runtime, bug.id);
+
+    var comments = yield waitForPullComments(runtime, 'autolander', 'autolander-test', pull.number);
+    var expected = require('./../lib/github').COMMENTS.INVALID_COMMIT_NO_BUG;
+    assert.equal(comments[0].body, expected);
+
+    yield waitForCheckinNeededRemoved(runtime, bug.id);
+  }));
+});
